ba72b18559c901be5d4c680c729f091e1cff27fd
Vermayugank <yugankv.ug20.ee@nitp.ac.in>
2022-04-11 15:30:49 +0530

    edit in question1(lab07)

--- src/com/CSlab/lab07/Question1.java
+++ src/com/CSlab/lab07/Question1.java
@@ -2,14 +2,27 @@ package com.CSlab.lab07;
 
 public class Question1 {
     public static void main(String[] args) {
-        int x =5;
         try {
+            int x =5;
             Integer y = Integer.valueOf(x);
-            System.out.println(y);
-
+            Integer w = x;     //autoboxing
+            System.out.println("Initial value :"+x+" Value after autoboxing :"+w);
+            boolean b = true;
+            Boolean B= Boolean.valueOf(b);
+            Boolean B1 = b;
+            System.out.println("Initial value :"+b+" Value after autoboxing :"+B1);
+            float f = 10.00f;
+            Float F = Float.valueOf(f);
+            Float F1= f;
+            System.out.println("Initial value :"+f+" Value after autoboxing :"+F1);
+            long l= 515336;
+            Long L = Long.valueOf(l);
+            Long L1= l;
+            System.out.println("Initial value :"+l+" Value after autoboxing :"+L1);
         }
         catch (Exception e){
             System.out.println(e);
         }
     }
+
 }
